fbdec5e29cdb7a19a646099371f1ed6e50a7c0f2
Grimsbear <107369678+Grimsbear@users.noreply.github.com>
2022-11-29 21:19:45 -0600

    working on animejs (#15)
    Co-authored-by: Mauvonte Roberts <moeroberts183@gmail.com>

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-let bodyStyle = document.querySelector('body')
-bodyStyle.animate
+// import anime from 'animejs/lib/anime.es.js';
+
+// const anime = require('animejs');
+
+
+anime({
+    targets: document.querySelector('group-info'),
+    translateX: 250,
+    scale: 2,
+    rotate: '90deg',
+    
+  });
+    
+
+
+
